9666452a3b8f7cdeb5bd8a1d1b0edbafc3ac8627
Dru <elena.druzhinina@gmail.com>
2021-05-18 14:02:45 +0500

    solved 3.1 greedy money change

--- week3_greedy_algorithms/1_money_change.py
+++ week3_greedy_algorithms/1_money_change.py
@@ -1,10 +1,26 @@
 # Uses python3
-import sys
+# Problem Description
+# Task:
+#   The goal in this problem is to find the minimum number of coins needed to change the input value (an integer) into coins with denominations 1, 5, and 10.
+# Input Format:
+#   The input consists of a single integer 𝑚 3
+# Constraints:
+#   1 ≤ 𝑚 ≤ 10 .
+# Output Format:
+#   Output the minimum number of coins with denominations 1, 5, 10 that changes 𝑚.
 
 def get_change(m):
-    #write your code here
-    return m
+
+    coins = [10, 5, 1]
+    coins_count, i = 0, 0
+    while m > 0:
+        if m < coins[i]:
+            i += 1
+        else:
+            coins_count += 1
+            m -= coins[i]
+    return coins_count
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
